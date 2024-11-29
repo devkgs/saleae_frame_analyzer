ea6acd11bea9ccec617d9b59553a4ed27cdfffc9
sylvain <sylvainkrieg@gmail.com>
2024-11-29 17:08:00 +0100

    Add parser

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
+[![Pylint](https://github.com/devkgs/saleae_frame_analyzer/actions/workflows/pylint.yml/badge.svg?branch=main)](https://github.com/devkgs/saleae_frame_analyzer/actions/workflows/pylint.yml)
+
 # saleae_frame_analyzer
-Extract frames from saleae export files
+A python script that extract SPI frames from Saleae Logic 2 App SPI export file.  
+
+
+# Prerequisites
+ - Saleae Logic 2 CSV output file
+ - Python3
+
+# Usage
+
+## Generating output from Saleae Logic 2
+See https://github.com/idaholab/Saleae_Output_Parser 
+
+## Running the script
+``python3 <path_to_output_file>``  
+
+You may use the ```-h``` flag to see the various options that are available. 
+
+The output will be written to stdout. Use the ```>``` or ```>>``` characters to send the output to a file.
+
+### Options
+```commandline
+  -h, --help     show this help message and exit
+  -a <reg_addr>  Addr of the reg to filter
+  -l             Print lines
+  -t             Print time
+  -s             Print summary
+```
+
+### Example
+Locate all frames with address 0xFC. Print line number, time and summary.  
+```python3 data/spi_export -a 0xFC -l -t -s```
+
+Output
+```commandline
+318 0.0032885 0xFC 0xFD
+319 0.0032985 0xFD 0xFE
+320 0.0033085 0xFE 0xFF
+
+638 0.0066165 0xFC 0xFD
+639 0.0066265 0xFD 0xFE
+640 0.0066365 0xFE 0xFF
+
+...
+
+8318 0.0864885 0xFC 0xFD
+8319 0.0864985 0xFD 0xFE
+8320 0.0865085 0xFE 0xFF
+
+-------------------------------------
+Frames found =  26
+Frames total =  1666
+-------------------------------------
+```
---
+++ reg_parser.py
@@ -0,0 +1,146 @@
+"""
+Doc
+
+input file:
+columns: spi, type, time, frame duration, mosi, miso
+type: enable, result, disable
+
+"""
+
+import argparse
+from abc import ABC, abstractmethod
+
+def add_arguments(argument_parser: argparse.ArgumentParser) -> None:
+    """
+    Add arguments to arg parser.
+    :param argument_parser:
+    :return:
+    """
+    argument_parser.description = "A python program to parse Saleae Logic Analyzer 2 export files."
+    argument_parser.add_argument('-a', dest='reg_addr', metavar='<reg_addr>', nargs='+',
+                                 help="Addr(s) of the reg to filter")
+    argument_parser.add_argument('-l', action='store_true', dest='print_lines', help='Print lines')
+    argument_parser.add_argument('-t', action='store_true', dest='print_time', help='Print time')
+    argument_parser.add_argument('-s', action='store_true', dest='print_summary',
+                                 help='Print summary')
+    argument_parser.add_argument(dest='in_file', metavar='FILE', help="Saleae export data file")
+
+class DataByte(): # pylint: disable=too-few-public-methods
+    """
+    Handle result data for one byte
+    """
+    def __init__(self, data_out='', data_in='', time='', line=0):
+        self.data_out = data_out
+        self.data_in = data_in
+        self.time = time
+        self.line = line
+
+    def __str__(self):
+        return f"{self.data_in} {self.data_out}"
+
+class Protocol(ABC):
+    """
+    Protocol abstract class.
+    """
+    def __init__(self, cmd_args):
+        self.file = cmd_args.in_file
+        self.addr_filter = set(cmd_args.reg_addr)
+        self.arg_print_line = cmd_args.print_lines
+        self.arg_print_time = cmd_args.print_time
+        self.frames = []
+        self.total_frames = 0
+
+    @abstractmethod
+    def read_file(self):
+        """
+        Read the complete file and store result in frames variable
+        :return:
+        """
+
+    @abstractmethod
+    def print_frames(self):
+        """
+        Print the data of the variable frames
+        :return:
+        """
+
+    def print_summary(self):
+        """
+
+        :return:
+        """
+        print("-------------------------------------")
+        print("Frames found = ", len(self.frames))
+        print("Frames total = ", self.total_frames)
+        print("-------------------------------------")
+
+
+class SPIProtocol(Protocol):
+    """
+    SPI specific protocol
+    """
+    def __init__(self, cmd_args):
+        super().__init__(cmd_args)
+        self.current_frame = []
+        #self.frames = []
+
+    def read_file(self):
+        first_frame = True
+        line_number = 0
+        addr_detected = False
+        with open(self.file, 'r', encoding="utf-8") as infile:
+            next(infile)  # Skip the first line
+            line_number = line_number + 1
+            for line in infile:
+                line = line.rstrip()  # remove whitespaces
+                line_number = line_number + 1
+                splitted_line = line.split(',')  # to array
+                if splitted_line[1] == '"enable"':
+                    enable_detected = True
+                    self.total_frames += 1
+                elif splitted_line[1] == '"disable"':
+                    if addr_detected :
+                        self.frames.append(self.current_frame[:])
+                        self.current_frame.clear()
+                    enable_detected = False
+                    first_frame = True
+                    addr_detected = False
+                elif splitted_line[1] == '"result"':
+                    if enable_detected:
+                        if first_frame:
+                            if splitted_line[4] in self.addr_filter:
+                                self.current_frame.append(DataByte(data_in = splitted_line[4],
+                                                                   data_out = splitted_line[5],
+                                                                   time = splitted_line[2],
+                                                                    line=line_number))
+                                addr_detected = True
+                            first_frame = False
+                        else:
+                            if addr_detected:
+                                self.current_frame.append(DataByte(data_in = splitted_line[4],
+                                                                   data_out = splitted_line[5],
+                                                                   time = splitted_line[2],
+                                                                    line=line_number))
+
+    def print_frames(self):
+        for element in self.frames:
+            for data_byte in element:
+                output = [data_byte.data_in, data_byte.data_out]
+                if self.arg_print_time:
+                    output.insert(0, data_byte.time)
+                if self.arg_print_line:
+                    output.insert(0, data_byte.line)
+                print(*output)
+            print() # a new line to improve visibility
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+    add_arguments(parser)
+    args = parser.parse_args()
+
+    analyzer = SPIProtocol(args)
+    analyzer.read_file()
+    analyzer.print_frames()
+
+    if args.print_summary:
+        analyzer.print_summary()
